353c5b3836269091c13df310cd2780da91eae96f
tslight <tobyslight@gmail.com>
2018-07-22 01:13:13 +0100

    lazygitpush

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yorn",
-    version="0.0.2",
+    version="0.0.3",
     author="Toby Slight",
     author_email="tobyslight@gmail.com",
     description="A boringly simple CLI input function.",
